8cc748fb72eaa661f2a02280c17c790c79d91ec2
annetteaune <annetteaune@outlook.com>
2023-04-22 17:08:30 +0200

    css

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,23 +1,26 @@
 import { Link } from "react-router-dom";
 
-
 export default function Profile({ user, login }) {
 	console.log(user);
 
 	if (login === true) {
 		return (
-			<section>
-				<h2>Welcome {user.username}!</h2>
+			<section className="profile-page">
+				{" "}
+				<div className="image-wrapper">
+					<img src={user.imageUrl} />
+				</div>
+				<h2>Welcome, {user.username}!</h2>
 				<span>{user.useremail}</span>
-				<img src={user.imageUrl} />
 			</section>
 		);
 	} else {
 		return (
 			<section>
 				<h2>You need to log in to see your profile</h2>
-                <Link to="/login">
-                <button>Log in</button></Link>
+				<Link to="/login">
+					<button>Log in</button>
+				</Link>
 			</section>
 		);
 	}
